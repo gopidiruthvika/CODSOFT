d1a77592c435111d640d2c8a2afb79ef7f127133
Gopidi Ruthvika <reddygopidiruthvika@gmail.com>
2025-09-29 21:04:42 +0530

    git commit -m "Added Task 2 simple calculator"
    in codsoft

---
+++ simplecalci.py
@@ -0,0 +1,25 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+print("Choose operation: +, -, *, /")
+operation = input("Enter operation: ")
+if operation == "+":
+    result = num1 + num2
+    print("Result:", result)
+
+elif operation == "-":
+    result = num1 - num2
+    print("Result:", result)
+
+elif operation == "*":
+    result = num1 * num2
+    print("Result:", result)
+
+elif operation == "/":
+    if num2 != 0:  
+        result = num1 / num2
+        print("Result:", result)
+    else:
+        print("Error: Cannot divide by zero!")
+
+else:
+    print("Invalid operation!")
